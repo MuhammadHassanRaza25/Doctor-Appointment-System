9b631d42b671b950f09bdcdfaa4ffa0f5e7efd93
MuhammadHassanRaza25 <hassanejaz773@gmail.com>
2024-12-06 04:12:39 +0500

    working on it

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import DoctorImage from "../images/doctor.jpg"
+import heroImage from "../images/heroimage.jpg"
 
 export default function HeroSection(){
     return(
@@ -7,13 +8,13 @@ export default function HeroSection(){
            <div className="heroSection bg-gray-50 pt-28 pb-36 flex px-24 justify-between items-center">
               
               <div className="heroDetail flex flex-col gap-9">
-                 <h1 className="heroHeading font-bold">Welcome to QuickHealth</h1>
+                 <h1 className="heroHeading flex items-center font-bold">Welcome to QuickHealth <span className="heroHeartImage w-[5vw]"><Image className="mix-blend-multiply" src={heroImage} alt="hero Image"/></span></h1>
                    <p className="heroDesc text-gray-500 w-[35vw]">
                      QuickHealth is an online platform that allows users to find doctors and book appointments effortlessly. It provides a seamless way to connect with healthcare professionals and manage medical consultations, ensuring convenience, reliability, and timely access to quality care. This platform is created to enhance your healthcare experience with ease and efficiency.
                    </p>
                  <div className="heroBtnDiv flex gap-5">
                     <button className="heroBtn bg-blue-600 text-white px-5 py-2.5 font-bold rounded-lg hover:bg-blue-700">Find Doctors</button>
-                    <button className="heroBtn border border-green-600 bg-white text-green-700 hover:bg-green-50 px-5 py-2.5 font-bold rounded-lg">Apply as Doctor</button>
+                    <button className="heroBtn border border-green-600 bg-gray-50 text-green-700 hover:bg-green-50 px-5 py-2.5 font-bold rounded-lg">Apply as Doctor</button>
                  </div>
               </div>
 
